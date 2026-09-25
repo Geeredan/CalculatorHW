e35abc813a824c73c1edac373b11c5a00cf051ab
Geeredan <vova.dubovyk2005@gmail.com>
2026-09-25 18:21:29 +0300

    Created program for Calculator

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter action (+,-,*,/) : ");
+string action  = Console.ReadLine();
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine("Result : " + (a + b)); 
+        break;
+
+    case "-": 
+        Console.WriteLine("Result : " + (a - b));
+        break;
+
+    case "*":
+        Console.WriteLine("Result : " + (a * b));
+        break;
+
+    case "/":
+        Console.WriteLine("Result : " + (a / b));
+        break;
+
+    default:
+        Console.WriteLine("Incorrect action!");
+        break;
+}
